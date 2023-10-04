6f1a3b82737b07fcc457700ce00d118a84e3f39a
[Clara Mahaux] <[claramahaux@gmail.com]>
2023-10-04 16:47:07 +0200

    TRAILER IS WORKING

--- frontend/src/components/pages/MoviePage.tsx
+++ frontend/src/components/pages/MoviePage.tsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import Movie from "../../types/movie";
 import axios from "axios";
+import ReactPlayer from 'react-player';
+import movieTrailer from 'movie-trailer';
 import "../../scss/pages/_moviePage.scss";
 
 export async function loader({ params }) {
@@ -22,13 +24,7 @@ function MoviePage() {
   //TODO: make it pretty and shine bright like a diamond, merci d'avance Clara et un coeur sur toi
   return (
     <article className="movieSheet">
-      <iframe
-        width="660"
-        height="415"
-        src={`https://www.youtube.com/embed/${movie.trailer}`}
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
+     <ReactPlayer url={movie.trailer} controls={true} />
       <h2 className="movieSheet__title">{movie.title}</h2>
       <h3 className="movieSheet__release_date group">{movie.release_date}</h3>
       <h3 className="movieSheet__duration group">{movie.duration}</h3>
